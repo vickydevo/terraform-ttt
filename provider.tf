37243871c9c001e7af690404b8c8942bb47bb32d
vignan2020 <88151516+vickydevo@users.noreply.github.com>
2023-04-30 16:47:31 +0530

    access and secrets removed

--- provider.tf
+++ provider.tf
@@ -14,8 +14,6 @@ terraform {
 
 
 provider "aws" {
-  # access_key = "AKIA3N2LTYY4XXXXXXXXX"
-  # secret_key = "XXXXXXXXXXXXXYmGRkc7iRMiiCS5lPCT"
   shared_config_files      = ["C:/Users/VIGNAN/.aws/config"]
   shared_credentials_files = ["C:/Users/VIGNAN/.aws/credentials"]
   profile                  = "default"
